3c7cafeda27e57b846b36df35b563c22d3703003
Flora <flora.e.hewitt@gmail.com>
2020-03-31 07:24:25 +0400

    fixed the classnames

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -8,11 +8,11 @@ const Home = () => (
   <Layout>
     <div id="homeback">
     </div>
-    <div class="main">
-      <div class="container">
-        <div class="row">
-          <div class="front-card col-xs-10 col-xs-offset-1">
-            <div class="col-xs-6 welcome">
+    <div className="main">
+      <div className="container">
+        <div className="row">
+          <div className="front-card col-xs-10 col-xs-offset-1">
+            <div className="col-xs-6 welcome">
               <div id="welcome-text">
                 <h1><strong>Welcome to Twitter.</strong></h1>
                 <p>Connect with your friends &#8212; and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
@@ -20,15 +20,15 @@ const Home = () => (
               <p><a href="#" id="twit-info">Hack Pacific - Backendium Twitter Project</a></p>
               <p><a href="#" id="twit-account">Tweet and photo by @Hackpacific<br/>3:20 PM - 15 December 2016</a></p>
             </div>
-            <div class="log-in col-xs-4 col-xs-offset-1">
+            <div className="log-in col-xs-4 col-xs-offset-1">
               <form>
-                <div class="form-group">
-                  <input type="text" class="form-control username" placeholder="Username"/>
+                <div className="form-group">
+                  <input type="text" className="form-control username" placeholder="Username"/>
                 </div>
-                <div class="form-group col-xs-8">
-                  <input type="password" class="form-control password" placeholder="Password"/>
+                <div className="form-group col-xs-8">
+                  <input type="password" className="form-control password" placeholder="Password"/>
                 </div>
-                <button id="log-in-btn" class="btn btn-default btn-primary col-xs-3 col-xs-offset-1">Log in</button>
+                <button id="log-in-btn" className="btn btn-default btn-primary col-xs-3 col-xs-offset-1">Log in</button>
                 <label>
                   <input type="checkbox"/>
                   <span>Remember me</span>
@@ -37,21 +37,21 @@ const Home = () => (
                 <a href="#">Forgot password?</a>
               </form>
             </div>
-            <div class="sign-up col-xs-4 col-xs-offset-1">
+            <div className="sign-up col-xs-4 col-xs-offset-1">
               <form>
-                <div class="new-to-t">
+                <div className="new-to-t">
                   <p><strong>New to Twitter?</strong><span> Sign Up</span></p>
                 </div>
-                <div class="form-group">
-                  <input type="text" class="form-control username" placeholder="Username"/>
+                <div className="form-group">
+                  <input type="text" className="form-control username" placeholder="Username"/>
                 </div>
-                <div class="form-group">
-                  <input type="email" class="form-control email" placeholder="Email"/>
+                <div className="form-group">
+                  <input type="email" className="form-control email" placeholder="Email"/>
                 </div>
-                <div class="form-group">
-                  <input type="password" class="form-control password" placeholder="Password"/>
+                <div className="form-group">
+                  <input type="password" className="form-control password" placeholder="Password"/>
                 </div>
-                <button id="sign-up-btn" class="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
+                <button id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
               </form>
             </div>
           </div>
--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -5,21 +5,21 @@ import './layout.scss';
 const Layout = (props) => {
   return (
     <React.Fragment>
-      <nav class="navbar-default navbar-fixed-top">
-        <div class="container">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-              <i class="fa fa-twitter"></i>
+      <nav className="navbar-default navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">
+              <i className="fa fa-twitter"></i>
             </a>
           </div>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">language: <strong>English </strong></a>
-              <ul class="dropdown-menu row" role="menu">
-                <li class="col-xs-12"><a href="#">Bahasa Malaya</a></li>
-                <li class="col-xs-12"><a href="#">Dansk</a></li>
-                <li class="col-xs-12"><a href="#">English</a></li>
-                <li class="col-xs-12"><a href="#">Suomi</a></li>
+          <ul className="nav navbar-nav navbar-right">
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">language: <strong>English </strong></a>
+              <ul className="dropdown-menu row" role="menu">
+                <li className="col-xs-12"><a href="#">Bahasa Malaya</a></li>
+                <li className="col-xs-12"><a href="#">Dansk</a></li>
+                <li className="col-xs-12"><a href="#">English</a></li>
+                <li className="col-xs-12"><a href="#">Suomi</a></li>
               </ul>
             </li>
           </ul>
